ff8e43edf5a746eb98dd39e352e67122eb4ab68e
Joshua Curl <curljosh@msu.edu>
2014-10-19 17:06:46 -0400

    Added content_type and title to browser

--- twine/browser.py
+++ twine/browser.py
@@ -3,8 +3,12 @@ from spynner import *
 class TwineBrowser(Browser):
     def __init__(self, debug_level):
         super(TwineBrowser, self).__init__(debug_level = debug_level)
+
         self.headers = [("Accept", "text/html; */*")]
         self._http_status = ""
+        self._content_type = ""
+        self._title = ""
+
         self._history = []
 
         # Last form modified by user
@@ -42,8 +46,12 @@ class TwineBrowser(Browser):
         return self._http_status
 
     @property
-    def history(self):
-        return self._history
+    def content_type(self):
+        return self._content_type
+
+    @property
+    def title(self):
+        return self._title
 
     def add_header(self, header):
         self.headers.append(header)
@@ -69,16 +77,20 @@ class TwineBrowser(Browser):
     def load(self, url, add_to_history = True):
         if url:
             old_url = self.url
-            self._http_statuses = {} 
+            self._http_statuses = {}
+            self._content_types = {}
 
             ret = super(TwineBrowser, self).load(url)
 
             if ret:
-                self._http_status = self._http_statuses.get(self.url, "")
                 if add_to_history and old_url:
                     self._history.append(old_url)
                 self.at_empty_page = False
                 self.last_form = None
+
+                self._http_status = self._http_statuses.get(self.url, "")
+                self._content_type = self._content_types.get(self.url, "")
+                self._title =  self.soup("title").text() or ""
 
             return ret
         else:
@@ -98,7 +110,7 @@ class TwineBrowser(Browser):
             # if this is a '?' or '/' URL, then assume that we want to tack it
             # onto the end of the current URL.
             try_urls.append(urlparse.urljoin(self.url, url))
-            
+
             for u in try_urls:
                 if self.load(u):
                     return True
@@ -110,8 +122,6 @@ class TwineBrowser(Browser):
     def _on_reply(self, reply):
         super(TwineBrowser, self)._on_reply(reply)
 
-        print self._reply_url
-
         try:
             http_status = "%s" % toString(
             reply.attribute(QNetworkRequest.HttpStatusCodeAttribute))
@@ -119,6 +129,16 @@ class TwineBrowser(Browser):
             http_status = ""
 
         self._http_statuses[self._reply_url] = http_status
+
+        try:
+            content_type = "%s" % toString(
+            reply.header(QNetworkRequest.ContentTypeHeader))
+        except:
+            content_type = ""
+
+        print content_type
+
+        self._content_types[self._reply_url] = content_type
 
     def back(self):
         if self._history:
