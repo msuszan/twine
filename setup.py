4bd325cf4650a2e20143411e5f641f39abd91498
Joshua Curl <curljosh@msu.edu>
2014-10-19 18:06:19 -0400

    Moved twine/ into src/ and renamed twine-sh to twine

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # setup information
 setup(name = 'twine',
       description = 'An implementation of twill based on spynner',
       author = 'Joshua Curl, C. Titus Brown, and Ben R. Taylor',
       author_email = 'curljosh@msu.edu',
-
       license='MIT',
-      packages = ['twine', 'twine.extensions',
-                  'twine.extensions.match_parse'],
-      # allow both 
-      entry_points = dict(console_scripts=['twine = twine.shell:main'],),
-      scripts = ['twill-fork'],
-
+      packages = find_packages('src'),
+      package_dir = { '': 'src' },
+      scripts = ['twine'],
       maintainer = 'Joshua Curl',
       maintainer_email = 'curljosh@msu.edu',
       url = 'https://github.com/joshuacurl/twine',
-      long_description = """\
-twill is an implementation of the twill scripting language using spynner. This will allow for some new features, most notably JavaScript support.
-""",
+      long_description = open("README.md").read(),
       classifiers = ['Development Status :: 4 - Beta',
                      'Environment :: Console',
                      'Intended Audience :: Developers',
