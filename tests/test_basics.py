7cc6acc4a19f0b20da4fcc0e7aaf2ea4ac962a86
Joshua Curl <curljosh@msu.edu>
2014-10-22 19:01:20 -0400

    More basic tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,10 +1,22 @@
 from twine.commands import *
-from test_server import test_server
 
 class TestBasics:
     def setUp(self):
         go('http://127.0.0.1:5000/')
+    def test_reload(self):
+        reload()
+    def test_url(self):
+        url('127.0.0.1:5000')
+    def test_code(self):
+        code('200')
+    def test_follow(self):
+        follow('link')
+        url('127.0.0.1:5000/link')
     def test_find(self):
         find('Hello World!')
-    def test_code(Self):
-        code('200')
+    def test_notfind(self):
+        notfind('Goodbye')
+    def test_back(self):
+        follow('link')
+        back()
+        find('Hello World!')
--- tests/test_server.py
+++ tests/test_server.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 test_server = Flask(__name__)
 
 @test_server.route("/")
 def index():
-  return "Hello World!"
+  return render_template("index.html")
+
+@test_server.route("/link")
+def link():
+  return render_template("link.html")
 
 if __name__ == "__main__":
     test_server.run()
